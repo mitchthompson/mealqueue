dbbbc911efa0e932dc57dffd813dc2ae727f943a
Mitch Thompson <thomitchell@gmail.com>
2018-05-07 14:34:51 -0700

    Adds a bottom nav to main activity.

--- android/MealQueue/app/src/main/java/com/mitchlthompson/mealqueue/MainActivity.java
+++ android/MealQueue/app/src/main/java/com/mitchlthompson/mealqueue/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
